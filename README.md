bd4b3c92b6b72a42d4ceefdccac8c4cf06480d4c
J. Tangelder <j.tangelder@gmail.com>
2014-01-17 11:13:17 +0100

    refactor, more comments

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ issuebot
 ========
 
 Close issues after x days
+
+Set the env variable `GITHUB_APP_TOKEN` with your Github App token, and update
+the `autoclose_repos` array with the repos you would like to be auto closed.
--- index.js
+++ index.js
@@ -4,53 +4,67 @@ var github = require('github');
 var now = new Date();
 var message = fs.readFileSync("close_msg.txt", {encoding:'utf-8'});
 
-function daysAgo(date) {
-  return Math.floor((now - new Date(date)) / 1000 / (3600*24));
-}
-
+// add your repos
 var autoclose_repos = [
   { days: 90, repo: 'hammer.js', user: 'eightmedia' }
 ];
+
 
 var client = new github({
     version: "3.0.0",
     timeout: 5000
 });
 
+// sign in
+// make sure GITHUB_APP_TOKEN is set
 client.authenticate({
     type: "oauth",
     token: process.env.GITHUB_APP_TOKEN
 });
 
+
+// calculate days
+function daysAgo(date) {
+  return Math.floor((now - new Date(date)) / 1000 / (3600*24));
+}
+
+// auto close when overdue
+function autoCloseIssue(issue, repo) {
+  if(daysAgo(issue.updated_at) > repo.days) {
+    client.issues.edit({
+      repo: repo.repo,
+      user: repo.user,
+      number: issue.number,
+      state: 'closed'
+    });
+
+    client.issues.createComment({
+      repo: repo.repo,
+      user: repo.user,
+      number: issue.number,
+      body: message
+    });
+
+    console.log('closed #'+ issue.number +' of '+ issue.user+'/'+issue.repo);
+  }
+}
+
+// walk repos
 autoclose_repos.forEach(function(repo) {
-  client.issues.repoIssues({
+  var options = {
     repo: repo.repo,
     user: repo.user,
     state: 'open',
     per_page: 200
-  }, function(err, issues) {
+  };
+
+  client.issues.repoIssues(options, function(err, items) {
     if(err) {
       return console.log(err);
     }
 
-    return issues.forEach(function(item) {
-      if(daysAgo(item.updated_at) > repo.days) {
-        client.issues.edit({
-          repo: repo.repo,
-          user: repo.user,
-          number: item.number,
-          state: 'closed'
-        });
-
-        client.issues.createComment({
-          repo: repo.repo,
-          user: repo.user,
-          number: item.number,
-          body: message
-        });
-
-        console.log('closed #'+ item.number +' of '+ repo.user+'/'+repo.repo);
-      }
+    return items.forEach(function(issue) {
+      autoCloseIssue(issue, repo);
     });
   });
 });
